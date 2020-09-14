65a514f5440f5ebf0c7d974b2fc7f3192049f212
joaorpereira <jprp17@live.com>
2020-09-14 00:02:39 -0300

    nav and info section - done

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from 'react'
+import { FaTimes, FaBars } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
+import { Button } from "../../globalStyles"
+import { Nav, NavContainer, NavLogo, NavIcon, MobileIcon, NavMenu , NavLinks, NavItem, NavItemBtn, NavBtnLink} from "./NavStyles"
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
+
+    const onClick = () => {
+        setClick(!click)
+    }
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton() 
+    }, [])
+
+    window.addEventListener("resize", showButton)
+
+    return (
+        <>
+            <IconContext.Provider value={{color: "#fff"}}>
+                <Nav>
+                    <NavContainer>
+                        <NavLogo to="/">
+                            <NavIcon/>
+                                ULTRA
+                        </NavLogo>
+                        <MobileIcon
+                            onClick = {onClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                        <NavMenu onClick={onClick} click={click}>
+                            <NavItem >
+                                <NavLinks to="/"> Home </NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="/services"> Services </NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="/products"> Products </NavLinks>
+                            </NavItem>
+                            <NavItemBtn>
+                                {button ? (
+                                    <NavBtnLink to="/sign-up">
+                                        <Button primary>Sign Up</Button>
+                                    </NavBtnLink>) : (
+                                    <NavBtnLink to="/sign-up">
+                                        <Button fontBig primary>Sign Up</Button>
+                                    </NavBtnLink>
+                                    )}
+                            </NavItemBtn>
+                        </NavMenu>
+                    </NavContainer>
+                </Nav>       
+            </IconContext.Provider>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home/Data.js
@@ -0,0 +1,63 @@
+export const homeObjOne = {
+    lightBg : false,
+    primary: true, 
+    imgStart: "", 
+    lightTopLine: false, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.", 
+    headline: "Lead Generation Specialist for Online Businesses", 
+    topLine: "Marketing Agency",
+    start: "", 
+    img: require("../../images/svg-1.svg"), 
+    alt: "Image",
+}
+
+export const homeObjTwo = {
+    lightBg : false,
+    primary: true, 
+    imgStart: "", 
+    lightTopLine: true, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.", 
+    headline: "Lead Generation Specialist for Online Businesses", 
+    topLine: "Marketing Agency",
+    start: "", 
+    img: require("../../images/profile.jpg"), 
+    alt: "Image",
+}
+
+export const homeObjThree = {
+    lightBg : false,
+    primary: true, 
+    imgStart: "", 
+    lightTopLine: true, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.", 
+    headline: "Lead Generation Specialist for Online Businesses", 
+    topLine: "Marketing Agency",
+    start: "", 
+    img: require("../../images/svg-2.svg"), 
+    alt: "Image",
+}
+
+export const homeObjFour = {
+    lightBg : false,
+    primary: true, 
+    imgStart: "", 
+    lightTopLine: true, 
+    lightText: true, 
+    lightTextDesc: true, 
+    buttonLabel: "Get Started", 
+    description: "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.", 
+    headline: "Lead Generation Specialist for Online Businesses", 
+    topLine: "Marketing Agency",
+    start: "", 
+    img: require("../../images/svg-3.svg"), 
+    alt: "Image",
+}
